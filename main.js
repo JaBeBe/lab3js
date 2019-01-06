2cd9d43ec565cfe8dbdde50c9fdf9aab9725b328
Jacek Bebak <jacek.a.bebak@gmail.com>
2019-01-06 15:36:25 +0100

    cosmetic upgrade v2

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-window.addEventListener('deviceorientation', DewInfo)
+let ball = document.querySelector('ball')
+const container = document.querySelector('.container')
 
-let ball = document.getElementById('ball')
-
-function DewInfo(e) {
+const moveBall = e => {
     let alpha = event.alpha
     let beta = event.beta
     let gamma = event.gamma
     let marLeft = ball.style.left
     let marTop = ball.style.top
 
-    console.log(alpha, beta, gamma)
-    console.log(marLeft, marTop)
+    // console.log(alpha, beta, gamma)
+    // console.log(marLeft, marTop)
 
     marLeft = gamma + alpha + 20
     marTop = beta + alpha + 20
 
-    ball.style.left = marLeft + 170 + "px"
-    ball.style.top = marTop + 170 + "px"
+    ball.style.left = `${marLeft+170}px`
+    ball.style.top = `${marTop+170}px`
 }
+window.addEventListener('deviceorientation', moveBall)
